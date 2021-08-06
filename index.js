2ca04f0884b343c186c382948698272621b8615e
justin-xia15 <83360815+justin-xia15@users.noreply.github.com>
2021-08-06 12:04:23 -0400

    Update for loop

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-/* Open when someone clicks on the span element */
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
 }
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
 
-for(let i = 0; i < 5; i++) {
-  document.querySelectorAll(".overlay-content")[i].addEventListener("click", function() {
+for(let i = 0; i < 4; i++) {
+  document.querySelectorAll(".overlay-btn")[i].addEventListener("click", function() {
     closeNav();
-  })
+  });
 }
